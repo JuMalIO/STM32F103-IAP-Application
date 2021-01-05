97502b6dcbffb62a635b5f213694061d76960365
JuMalIO <spamjulius@mail.com>
2021-01-05 19:40:21 +0200

    Revert "delete bin"
    This reverts commit 02ba16f1912c8cf2a5a75e1132b35e69247f5e23.

--- Core/app.c
+++ Core/app.c
@@ -1,31 +1,12 @@
 #include "app.h"
-#include "main.h"
 #include "gpio.h"
-#include "msd.h"
-
-#define FLASH_FILE "update.bin"
-
-static _Bool LedBlink;
+#include "timer.h"
 
 void APP_Init(void)
 {
-	MSD_Init();
+	TIMER_Init();
 }
 
 void APP_Run(void)
 {
-	if (MSD_IsExistingFile(FLASH_FILE))
-	{
-		MSD_DeleteFile(FLASH_FILE);
-		
-		for (uint16_t i = 0; i < 100; i++)
-		{
-			GPIO_SetLed(i % 2);
-			HAL_Delay(50);
-		}
-	}
-	
-	LedBlink = !LedBlink;
-	GPIO_SetLed(LedBlink);
-	HAL_Delay(500);
 }
